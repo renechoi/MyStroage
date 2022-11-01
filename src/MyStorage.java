832f1f6be1b4d5e1fa1b5b5bae3b60125038a048
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-01 23:15:15 +0900

    Feat : create show menu and select menu

--- src/MyStorage.java
+++ src/MyStorage.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class MyStorage {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
+	static void showMenu(){
+		System.out.println("MyStorage V1");
+		System.out.println("------------");
+		System.out.println("1. register item info");
+		System.out.println("2. stock items (stocking)");
+		System.out.println("3. release items (releasing)");
+		System.out.println("4. view inventory");
+		System.out.println("5. exit");
+		System.out.println("------------");
 	}
+
+	static int selectMenu(Scanner scan){
+		System.out.println("Choose the menu (number) :");
+		int select = scan.nextInt();
+		return select;
+	}
+
 }
